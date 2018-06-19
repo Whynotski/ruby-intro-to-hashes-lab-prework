95082c0bfa8dec65d72cb35044618ae4bc6aa7e0
whynotski <laurawynosky@gmail.com>
2018-06-19 14:28:53 -0400

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,14 +1,16 @@
+#require pry
+
 def new_hash
 	hash_it = {}
 end
-
+#binding pry
 def actor
 	actor ={name:"Dwayne The Rock Johnson"}
 end
 
 def monopoly
 	monopoly = {
-		railroads:{ }
+		railroads:{ },
 	}
 end
 
@@ -16,6 +18,7 @@ def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	monopoly = {
 		railroads:{pieces: 4, names: {}, rent_in_dollars: {}}
+}
 end
 
 
@@ -24,9 +27,8 @@ def monopoly_with_third_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 	monopoly = {
 		railroads:{pieces: 4,
-			 names: {reading_railroad:{}, pennsylvania_railroad:{, b_and_o_railroad:{}, shortline:{}},
-				 rent_in_dollars: { one_piece_owned: 25, two_pieces_owned: 50, three_ieces_owned: 100, four_pieces_owned:200}
-
+			 names: {reading_railroad:{}, pennsylvania_railroad:{}, b_and_o_railroad:{}, shortline:{}},
+				 rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned:200},
 					 }
 				 }
 end
@@ -38,9 +40,9 @@ def monopoly_with_fourth_tier
 			 names: {reading_railroad:{"mortgage_value" => "$100"},
 			 pennsylvania_railroad:{"mortgage_value" => "$200"},
 				 b_and_o_railroad:{"mortgage_value" => "$400"},
-				 shortline:{"mortgage_value" => "$800"}
+				 shortline:{"mortgage_value" => "$800"},
+				 rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_ieces_owned: 100, four_pieces_owned:200},
 			 },
-				 rent_in_dollars: { one_piece_owned: 25, two_pieces_owned: 50, three_ieces_owned: 100, four_pieces_owned:200}
-					 }
-				 }
+	}
+}
 end
